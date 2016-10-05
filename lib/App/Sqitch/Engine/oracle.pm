7bbbc7605755d612e7c15a72519d53733107edd5
Johann Wilfling <johann.wilfling@gmail.com>
2016-10-05 11:11:44 +0200

    Fixes an Oracle database 10.2.0.1.0 bug
    See issue #316

--- lib/App/Sqitch/Engine/oracle.pm
+++ lib/App/Sqitch/Engine/oracle.pm
@@ -136,7 +136,15 @@ sub _log_conflicts_param {
 }
 
 sub _ts2char_format {
-    q{to_char(%1$s AT TIME ZONE 'UTC', '"year":YYYY:"month":MM:"day":DD') || to_char(%1$s AT TIME ZONE 'UTC', ':"hour":HH24:"minute":MI:"second":SS:"time_zone":"UTC"')}
+    #    q{to_char(%1$s AT TIME ZONE 'UTC', '"year":YYYY:"month":MM:"day":DD') || to_char(%1$s AT TIME ZONE 'UTC', ':"hour":HH24:"minute":MI:"second":SS:"time_zone":"UTC"')}
+    q{to_char(%1$s AT TIME ZONE 'UTC', '"year":YYYY')
+    || to_char(%1$s AT TIME ZONE 'UTC', ':"month":MM')
+    || to_char(%1$s AT TIME ZONE 'UTC', ':"day":DD')
+    || to_char(%1$s AT TIME ZONE 'UTC', ':"hour":HH24')
+    || to_char(%1$s AT TIME ZONE 'UTC', ':"minute":MI')
+    || to_char(%1$s AT TIME ZONE 'UTC', ':"second":SS')
+    || ':time_zone:UTC'
+    }
 }
 
 sub _ts_default { 'current_timestamp' }
